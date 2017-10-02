a60e1c818f7da2b42816555367b6f5ad2b88b840
nandocp <fcpontes@gmail.com>
2017-10-02 02:26:24 -0300

    posts finalizado

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,44 @@
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all
+	def new
+		@post = Post.new
 	end
 
-	def show
-		@post = Post.find_by(:id)
+	def create
+		@post = Post.new(post_params)
+		flash[:success] = "Post publicado com sucesso" if @post.save
+		redirect_to @post
 	end
 
 	def edit
-		@post = Post.
+		@post = Post.find(params[:id])
 	end
+	
+	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes(post_params)
+			flash[:success] = "! Post atualizado !"
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def index
+		@posts = Post.paginate(page: params[:page])
+	end
+
+	def destroy
+		Post.find(params[:id]).destroy
+		flash[:success] = "! Post deletado !"
+		redirect_to root_url
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:titulo, :corpo)
+		end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-	validates :titulo , presence: { message: "! deve ser escolhido !" }
-	validates :corpo  , presence: { message: "! não pode estar em branco !" }
+	validates :titulo , presence: { message: "! Não pode estar em branco !" }
+	validates :corpo  , presence: { message: "! Não pode estar em branco !" }
 
 	has_many :comentarios
 end
